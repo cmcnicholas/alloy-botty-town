d636677ef28ff482ff69ec5e823d209d4ea04e9f
Craig McNicholas <craig.mcnicholas@yotta.co.uk>
2020-07-23 10:42:38 +0100

    adding background music

---
+++ Assets/Scripts/AudioLooper.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioLooper : MonoBehaviour
+{
+    public List<AudioClip> Clips;
+    private AudioSource _audioSource;
+    private int _currentlyPlayingIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get the audio source to play on
+        _audioSource = GetComponent<AudioSource>();
+
+        // start looping
+        if (Clips != null && Clips.Count > 0)
+        {
+            StartCoroutine(LoopAudioCoroutine());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator LoopAudioCoroutine()
+    {
+        while (true)
+        {
+            _audioSource.clip = Clips[_currentlyPlayingIndex];
+            _audioSource.Play();
+            yield return new WaitForSeconds(_audioSource.clip.length);
+
+            // increment to next track
+            _currentlyPlayingIndex++;
+            if (_currentlyPlayingIndex >= Clips.Count)
+            {
+                // go back to start
+                _currentlyPlayingIndex = 0;
+            }
+        } 
+    }
+}
