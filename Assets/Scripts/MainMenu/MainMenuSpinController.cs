28d7afa549fbd07ec7deedab7a9e59ec76a71912
Craig McNicholas <craig.mcnicholas@yotta.co.uk>
2020-07-27 18:31:42 +0100

    added hints to the menu

---
+++ Assets/Scripts/MainMenu/MainMenuSpinController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MainMenuSpinController : MonoBehaviour
+{
+    public float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 1f, 0f), Speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/MainMenu/MainMenuTweenController.cs
+++ Assets/Scripts/MainMenu/MainMenuTweenController.cs
@@ -6,24 +6,41 @@ public class MainMenuTweenController : MonoBehaviour
     public GameObject MainMenuAlloyExperiment;
     public GameObject MainMenuTitle;
     public GameObject MainMenuOptions;
+    public GameObject MainMenuHintFixJobs;
+    public GameObject MainMenuHintFixInspections;
+    public GameObject MainMenuHintRegisterDefects;
+    public GameObject MainMenuHintSelection;
+    public float OffscreenMoveValue = 500f;
 
     // Start is called before the first frame update
     void Start()
     {
         // initially set to transparent
-        var renderer = MainMenuAlloyExperiment.GetComponent<Renderer>();
-        var colour = renderer.material.color;
-        renderer.material.color = new Color(colour.r, colour.g, colour.b, 0f);
+        var alloyExperimentRenderer = MainMenuAlloyExperiment.GetComponent<Renderer>();
+        var alloyExperimentColour = alloyExperimentRenderer.material.color;
+        alloyExperimentRenderer.material.color = new Color(alloyExperimentColour.r, alloyExperimentColour.g, alloyExperimentColour.b, 0f);
 
         // run and tween
-        StartCoroutine(FadeInCoroutine());
+        StartCoroutine(FadeInAlloyExperimentCoroutine());
         
-        // entry punch!
+        // entry punch for title
         iTween.PunchScale(MainMenuTitle, new Vector3(10f, 10f, 10f), 1.0f);
-        StartCoroutine(PunchCoroutine());
+        StartCoroutine(PunchTitleCoroutine());
 
-        // hide options
+        // hide options intially
         MainMenuOptions.SetActive(false);
+
+        // set scales
+        MainMenuHintFixJobs.transform.localScale = Vector3.zero;
+        MainMenuHintFixInspections.transform.localScale = Vector3.zero;
+
+        // set initial offscreen for some hints
+        iTween.MoveAdd(MainMenuHintRegisterDefects, new Vector3(-OffscreenMoveValue, 0, 0), 0f);
+        iTween.MoveAdd(MainMenuHintSelection, new Vector3(OffscreenMoveValue, 0, 0), 0f);
+
+        // cycle hints
+        StartCoroutine(CycleHintsPage1Coroutine());
+        StartCoroutine(CycleHintsPage2Coroutine());
     }
 
     // Update is called once per frame
@@ -32,21 +49,63 @@ public class MainMenuTweenController : MonoBehaviour
 
     }
 
-    private IEnumerator FadeInCoroutine()
+    private IEnumerator FadeInAlloyExperimentCoroutine()
     {
         yield return new WaitForSeconds(1f);
         iTween.FadeTo(MainMenuAlloyExperiment, 1f, 2f);
     }
 
-    private IEnumerator PunchCoroutine()
+    private IEnumerator PunchTitleCoroutine()
     {
         yield return new WaitForSeconds(1f);
+
+        // main menu options become available
         MainMenuOptions.SetActive(true);
 
         while (true)
         {
             yield return new WaitForSeconds(3.0f);
             iTween.PunchScale(MainMenuTitle, new Vector3(0, 0, 1f), 1.0f);
+        }
+    }
+
+    private IEnumerator CycleHintsPage1Coroutine()
+    {
+        yield return new WaitForSeconds(2f);
+
+        while (true)
+        {
+            // animate in
+            iTween.ScaleTo(MainMenuHintFixJobs, new Vector3(1f, 1f, 1f), 1f);
+            iTween.ScaleTo(MainMenuHintFixInspections, new Vector3(1f, 1f, 1f), 1f);
+
+            yield return new WaitForSeconds(5f);
+
+            // animate out
+            iTween.ScaleTo(MainMenuHintFixJobs, Vector3.zero, 1f);
+            iTween.ScaleTo(MainMenuHintFixInspections, Vector3.zero, 1f);
+
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+    private IEnumerator CycleHintsPage2Coroutine()
+    {
+        yield return new WaitForSeconds(2f);
+
+        while (true)
+        {
+            yield return new WaitForSeconds(5f);
+
+            // animate in
+            iTween.MoveAdd(MainMenuHintRegisterDefects, new Vector3(OffscreenMoveValue, 0f, 0f), 1f);
+            iTween.MoveAdd(MainMenuHintSelection, new Vector3(-OffscreenMoveValue, 0f, 0f), 1f);
+
+            yield return new WaitForSeconds(5f);
+
+            // animate out
+            iTween.MoveAdd(MainMenuHintRegisterDefects, new Vector3(-OffscreenMoveValue, 0f, 0f), 4f);
+            iTween.MoveAdd(MainMenuHintSelection, new Vector3(OffscreenMoveValue, 0f, 0f), 4f);
         }
     }
 }
