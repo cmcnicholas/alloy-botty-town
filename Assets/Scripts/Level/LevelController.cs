6b99806e560896dfe5c3eb0fad51d97033435446
Craig McNicholas <craig.mcnicholas@yotta.co.uk>
2020-07-27 18:45:39 +0100

    fixed timer counting down even when paused

--- Assets/Scripts/Level/LevelController.cs
+++ Assets/Scripts/Level/LevelController.cs
@@ -8,12 +8,12 @@ public class LevelController : MonoBehaviour
     public GameObject ScoreCounter;
     public int WarnSecondsRemaining;
     public float WarnSecondsRemainingColourOffsetY = 0.7f;
+    public int LevelTime = 300;
     private LevelSoundEffectsController _levelSoundEffectsController;
     private HudCounterController _timerCounterController;
     private HudCounterController _scoreCounterController;
-    private int _levelTime = 300;
     private int _levelScore = 0;
-    private float _startTime;
+    private float _timeLeft;
     private float _startColourTilingOffsetY;
 
     // this holds all our models e.g. assets/jobs/inspections
@@ -26,8 +26,7 @@ public class LevelController : MonoBehaviour
         _timerCounterController = TimerCounter.GetComponent<HudCounterController>();
         _scoreCounterController = ScoreCounter.GetComponent<HudCounterController>();
 
-        // log start time
-        _startTime = Time.time;
+        _timeLeft = LevelTime;
 
         // log starting colour
         _startColourTilingOffsetY = _timerCounterController.ColourTilingOffsetY;
@@ -37,13 +36,13 @@ public class LevelController : MonoBehaviour
     void Update()
     {
         // update the time left in the game
-        int timeLeft = _levelTime - (int)Math.Ceiling(Time.time - _startTime);
-        _timerCounterController.Number = timeLeft;
+        _timeLeft -= Time.deltaTime;
+        _timerCounterController.Number = (int)Math.Ceiling(_timeLeft);
 
-        bool isWarning = timeLeft <= WarnSecondsRemaining;
+        bool isWarning = _timeLeft <= WarnSecondsRemaining;
         _timerCounterController.ColourTilingOffsetY = isWarning ? WarnSecondsRemainingColourOffsetY : _startColourTilingOffsetY;
 
-        if (timeLeft > 0 && isWarning)
+        if (_timeLeft > 0 && isWarning)
         {
             _levelSoundEffectsController.PlayTimerCountdown();
         }
